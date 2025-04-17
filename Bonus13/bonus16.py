d1485310e56c4753a2012923fa2e186d665c030f
ngoquang110 <quangfcb@gmail.com>
2025-04-17 23:57:40 +0300

    Test GUI with API lần 2 có hơi lỗi nhưng mà lưu đã

--- Bonus13/bonus16.py
+++ Bonus13/bonus16.py
@@ -1,18 +1,25 @@
 import FreeSimpleGUI as sg
 import shutil
+import os
 
-layout = [[sg.Text('Select file to compress: '),sg.Input(tooltip='path to file',key='in_path'),sg.Button('Chose')],
-          [sg.Text('Select destination folder: '),sg.Input(tooltip='path to file',key='out_path'),sg.Button('Chose')],
+layout = [[sg.Text('Select file to compress: '),sg.Input(tooltip='path to file',key='in_path'),sg.FilesBrowse('Choose')],
+          [sg.Text('Select destination folder: '),sg.Input(tooltip='path to folder',key='out_path'),sg.FolderBrowse('Choose')],
           [sg.Button('Compress'),sg.Text(key='notice')]]
 
 window = sg.Window('File Zipper',layout)
 while True:
     event, values = window.read()
-    path_compress = values['in_path']
-    path_for_save = values['out_path']
     if event == 'Compress':
-        shutil.make_archive(path_for_save,'zip',path_compress)
-    elif event == sg.WINDOW_CLOSED():
+        path_compress = values['in_path']
+        path_for_save = values['out_path']
+        print(path_compress)
+        print(path_for_save)
+        if os.path.exists(path_compress) and os.path.exists(path_for_save):
+            shutil.make_archive(path_for_save,'zip',path_compress)
+            window['notice'].update('File compressed successfully!')
+        else:
+            window['notice'].update('Please select valid file and folder paths.')
+    if event == sg.WINDOW_CLOSED():
         break
 
 window.close()
