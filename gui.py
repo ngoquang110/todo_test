aa71138a5c01a19d9ec617665bf896feb34a13fe
ngoquang110 <quangfcb@gmail.com>
2025-04-29 23:41:30 +0300

    Implement the Edit button

--- gui.py
+++ gui.py
@@ -2,22 +2,43 @@ import functions
 import FreeSimpleGUI as sg
 
 # Define the window's contents
-layout = [[sg.Text("What's your name?")],
-          [sg.Input(tooltip='Enter todo',key='-INPUT-')],
-          [sg.Text(key='-OUTPUT-')],
-          [sg.Button('Ok'), sg.Button('Quit')]]
+layout = [[sg.Text("What do u want to do?")],
+          [sg.Input(tooltip='Enter todo',key='todo'),sg.Button('Add')],
+          [sg.Listbox(values=functions.get_todos(), key='todos', enable_events=True, size=[45,10]),sg.Button('Edit')],
+          [sg.Button('Quit')]]
 
 # Create the window
-window = sg.Window('Window Title', layout)
+window = sg.Window('Window Title', layout, font = ('Helvetica',20))
 
 # Display and interact with the Window using an Event Loop
 while True:
     event, values = window.read()
-    # See if user wants to quit or window was closed
-    if event == sg.WINDOW_CLOSED or event == 'Quit':
-        break
-    # Output a message to the window
-    window['-OUTPUT-'].update('Hello ' + values['-INPUT-'] + "! Thanks for trying FreeSimpleGUI")
+    print(event)
+    print(values)
+    # See if user wants to quit or window was close
+    match event:
+        case 'Add':
+            todos = functions.get_todos()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+            window['todos'].update(values = todos)
+        case 'Edit':
+            todo = values['todos'][0]
+
+            todos = functions.get_todos()
+            index = todos.index(todo)
+            todos[index] = values['todo'] + '\n'
+
+            functions.write_todos(todos)
+            #Update
+            window['todos'].update(values = todos)
+        case 'todos':
+            window['todo'].update(value = values['todos'][0])
+        case sg.WINDOW_CLOSED:
+            break
+        case 'Quit':
+            break
 
 # Finish up by removing from the screen
 window.close()
--- try.py
+++ try.py
@@ -1,3 +1,18 @@
-filenames = ["report.txt", "downloads.txt", "success.txt", "folders.txt"]
-for x in filenames:
-    print(x[:-4].capitalize())
+from functools import reduce
+from itertools import chain
+numbers  = [1,2,3,4,5,6]
+new_list = list(filter(lambda x : x > 3,numbers))
+print(new_list)
+
+produce = reduce(lambda x,y : x+y,numbers)
+print(produce)
+
+nested_list = [[1, 2], [3, 4], [5, 6]]
+
+# Sử dụng map và chain để flatten
+flat_map_result = list(chain.from_iterable(map(lambda x: x, nested_list)))
+print(flat_map_result)  # Kết quả: [1, 2, 3, 4, 5, 6]
+
+#flat
+flat_result = [item for sublist in nested_list for item in sublist]
+print(flat_result)  # Kết quả: [1, 2, 3, 4, 5, 6]
